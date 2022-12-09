d3a55108981b8c7ef16da7bb98748e067476166c
plagaz <plagaz66@gmail.com>
2022-12-09 11:30:56 -0400

    Mostrando el carro al Usuario

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ class App extends Component {
       { name: "Lechuga", price: 500, img: "/productos/lechuga.jpg" },
     ],
     carro: [],
+    esCarroVisible: false,
   };
 
   agregarAlCarro = (producto) => {
@@ -35,10 +36,22 @@ class App extends Component {
     });
   };
 
+  mostrarCarro = () => {
+    if(!this.state.carro.length){
+      return
+    }
+    this.setState({ esCarroVisible: !this.state.esCarroVisible });
+  };
+
   render() {
+    const { esCarroVisible } = this.state;
     return (
       <div>
-        <Navbar carro={this.state.carro}/>
+        <Navbar
+          carro={this.state.carro}
+          esCarroVisible={esCarroVisible}
+          mostrarCarro={this.mostrarCarro}
+        />
         <Layout>
           <Title />
           <Productos
